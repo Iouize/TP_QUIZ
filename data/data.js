9983c1823f04176243df693ae5318cd060889a11
Bonnet Manon <21701497t>
2020-02-19 14:01:47 +0100

    ajout quiz et questions

---
+++ data/data.js
@@ -0,0 +1,91 @@
+export const liste_theme = [
+    {
+        id :1,
+        titre: 'Histoire',
+    },
+
+    {
+        id:2,
+        titre: 'Peinture',
+    },
+
+    {
+        id:3,
+        titre:'Football',
+    },
+
+    {
+        id:4,
+        titre:'Rubicks Cube',
+    },
+
+    {
+        id:5,
+        titre:'Musique'
+    }
+]
+
+export const liste_quiz = [
+    {
+        id:1,
+        titre:'Les grands monarques',
+        idTheme:1,
+    },
+
+    {
+        id:2,
+        titre:'Waterloo',
+        idTheme:1,
+    },
+
+    // Faire la suite
+    {
+        id:4,
+        titre:'Van Gogh',
+        idTheme: 2,
+    },
+
+    {
+        id:5,
+        titre: 'Impressionnisme',
+        idTheme: 2,
+    },
+
+
+    {
+        id: 6,
+        titre: 'Musées parisiens',
+        idTheme: 2,
+    },
+
+
+    {
+        id: 7,
+        titre: 'Coupe du Monde 2018',
+        idTheme: 3,
+    },
+
+
+    {
+        id: 8,
+        titre: 'Clubs de football',
+        idTheme: 3,
+    },
+
+
+    {
+        id: 9,
+        titre: 'Année 2016',
+        idTheme: 3,
+    },
+
+
+    {
+        id: 10,
+        titre: 'Championnats',
+        idTheme: 4,
+    },
+
+
+    
+]
